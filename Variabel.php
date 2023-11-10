60d903b2d937f4b8dd943faf775ea9cc94e03cfe
apriani19 <145426970+apriani19@users.noreply.github.com>
2023-11-10 10:15:26 +0700

    Add files via upload

---
+++ Variabel.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = "hello";
+$hello= "Hello World!";
+
+// menampilkan isi variabel $a 
+//hello 
+echo $a . "</br>";
+
+// menampilkan isi variabel $a 
+// Hello world! 
+echo $hello ."</br>";
+
+// menampilkan isi dari variabel dengan nama yang sama seperti isi variabel $a
+// isi variabel $a = hello. jadi, nanti akan menampilkan isi dari variabel
+$hello
+// Hello World 
+echoa $$a . "</br>";
+
+?>
