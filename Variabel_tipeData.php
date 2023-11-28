8a76800887caefa3d13321b770242166d102aaf9
apriani19 <145426970+apriani19@users.noreply.github.com>
2023-11-28 07:12:45 +0700

    Variabel_tipeData.php

---
+++ Variabel_tipeData.php
@@ -0,0 +1,15 @@
+<?php
+
+$nrp = "173040888";
+$nama ="lorem ipsum";
+$umur = 20;
+$Nilai = 75,75;
+
+echo "Nrp:". $nrp . "</br>";
+echo "Nama: $nama</br>";
+echo "Umur:".$umur; print "</br>";
+
+// menampilkan bilangan real dengan format 3 digit dibelakang koma
+printf ("Nilai: %.3f</br>", $nilai);
+
+?>
